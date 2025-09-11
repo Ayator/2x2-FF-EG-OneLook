dfbf283c704aa273af84cf54b7f8a08bb077282e
Ayator <victor.d.ayala.2001@gmail.com>
2025-09-11 08:33:38 -0300

    Utils updated

---
+++ frontend/src/utils/locationUtils.js
@@ -0,0 +1,7 @@
+export const egImagesFolder = './eg_cases';
+
+export const ollCasesFolder = './oll_cases';
+
+export const assetsPath = "https://raw.githubusercontent.com/Ayator/2x2-FF-EG-OneLook/main/assets";
+
+export const jsonUrl = `${assetsPath}/algs_numbers.json`;
--- frontend/src/utils/ollUtils.js
+++ frontend/src/utils/ollUtils.js
@@ -1,3 +1,18 @@
+import { ollCasesFolder } from  "./locationUtils"
+
+// Helper: mapping OLL name to file code
+export const OLL_CODES = {
+  SUNE: "S",
+  ANTISUNE: "A",
+  T: "T",
+  L: "L",
+  U: "U",
+  PI: "P",
+  H: "H"
+};
+
+export const ROTATION_DEGREES = { F: 0, L: 90, B: 180, R: 270 };
+
 export function ollImage(oll, orientation = "F") {
-  return `/oll_cases/${oll}_${orientation}.jpg`;
+  return `/${ollCasesFolder}/${oll}_${orientation}.jpg`;
 }
